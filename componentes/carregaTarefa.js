85b5acde1389ad2414911bb4ac7a5a655a58f1ae
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-10-04 19:36:07 -0300

    create arquivo criaData

--- componentes/carregaTarefa.js
+++ componentes/carregaTarefa.js
@@ -1,4 +1,4 @@
-import { Tarefa } from "./criaTarefa.js"
+import { criaData } from "./criaData.js"
 
 export const carregaTarefa = () => {
     const lista = document.querySelector('[data-list]')
@@ -7,7 +7,8 @@ export const carregaTarefa = () => {
 
     lista.innerHTML = " "
     tarefasCadastradas.forEach((tarefa) => {
-        lista.appendChild(Tarefa(tarefa))
+        const dia = moment(tarefa.dataFormatada, 'DD/MM/YYYY')
+        lista.appendChild(criaData(dia))
     })
 
 }
