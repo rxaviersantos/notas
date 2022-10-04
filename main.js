add7cb17d01056d3cb210180a71403ef10686f36
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-10-03 22:51:06 -0300

    armazenando dados

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 import BotaoConclui from './componentes/concluiTarefa.js'
 import BotaoDeleta from './componentes/deletaTarefa.js'
+ 
 
 const handleNovoItem = (evento) => {
     evento.preventDefault()
 
     const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
-    const valor = input.value    
-         
+    const valor = input.value
+
     const calendario = document.querySelector('[data-form-date]')
     const data = moment(calendario.value)
-    
-    const dataFormatda = data.format('DD/MM/YYYY')
 
-    const dados = {
+    const dataFormatada = data.format('DD/MM/YYYY')
+
+    const dados = { 
         valor,
-        dataFormatda
+        dataFormatada
     }
 
     const criaTarefa = criarTarefa(dados)
 
     lista.appendChild(criaTarefa)
-    input.value = " "
 
+    sessionStorage.setItem('tarefas', JSON.stringify(dados) )
+    input.value = " "
 }
- 
+
 const criarTarefa = ({ valor, dataFormatada }) => {
+
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
     const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
@@ -34,9 +37,8 @@ const criarTarefa = ({ valor, dataFormatada }) => {
 
     tarefa.appendChild(BotaoConclui())
     tarefa.appendChild(BotaoDeleta())
-
-    return tarefa 
-  
+   
+    return tarefa
 
 }
 
