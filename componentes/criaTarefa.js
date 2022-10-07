8d86e8cf9f163cf8df53bf7232a5ab203f351114
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-10-06 23:12:48 -0300

    concluindo uma nota

--- componentes/criaTarefa.js
+++ componentes/criaTarefa.js
@@ -14,10 +14,12 @@ export const handleNovoItem = (evento) => {
     const data = moment(calendario.value)
 
     const dataFormatada = data.format('DD/MM/YYYY')
+    const concluida = false 
 
     const dados = { 
         valor,
-        dataFormatada
+        dataFormatada, 
+        concluida
     }
 
     const tarefasAtualizadas = [...tarefas, dados]
@@ -28,15 +30,21 @@ export const handleNovoItem = (evento) => {
     carregaTarefa()
 }
 
-export const Tarefa = ({ valor, dataFormatada }) => {
+export const Tarefa = ({ valor, dataFormatada, concluida }, id) => {
 
     const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
+    
     const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
+    if(concluida) {
+        tarefa.classList.add('done')
 
+    }
+
+    tarefa.classList.add('task')
+    
     tarefa.innerHTML = conteudo
 
-    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoConclui(carregaTarefa, id))
     tarefa.appendChild(BotaoDeleta())
    
     return tarefa
