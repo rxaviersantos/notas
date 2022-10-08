941a65219be1715946f402a1767490e70e56f797
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-10-07 23:47:04 -0300

    create delete notas

--- componentes/criaTarefa.js
+++ componentes/criaTarefa.js
@@ -7,18 +7,21 @@ export const handleNovoItem = (evento) => {
     evento.preventDefault()
 
     const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+    
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
 
     const calendario = document.querySelector('[data-form-date]')
     const data = moment(calendario.value)
 
+    const horario = data.format('HH:mm')
     const dataFormatada = data.format('DD/MM/YYYY')
     const concluida = false 
 
     const dados = { 
         valor,
-        dataFormatada, 
+        dataFormatada,
+        horario, 
         concluida
     }
 
@@ -30,11 +33,11 @@ export const handleNovoItem = (evento) => {
     carregaTarefa()
 }
 
-export const Tarefa = ({ valor, dataFormatada, concluida }, id) => {
+export const Tarefa = ({ valor, horario, concluida }, id) => {
 
     const tarefa = document.createElement('li')
     
-    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
+    const conteudo = `<p class="content">${horario} * ${valor}</p>`
     if(concluida) {
         tarefa.classList.add('done')
 
@@ -45,7 +48,7 @@ export const Tarefa = ({ valor, dataFormatada, concluida }, id) => {
     tarefa.innerHTML = conteudo
 
     tarefa.appendChild(BotaoConclui(carregaTarefa, id))
-    tarefa.appendChild(BotaoDeleta())
+    tarefa.appendChild(BotaoDeleta(carregaTarefa, id))
    
     return tarefa
 
--- componentes/deletaTarefa.js
+++ componentes/deletaTarefa.js
@@ -1,21 +1,21 @@
-const BotaoDeleta = () => { 
+const deletarTarefa = (atualiza, id) => { 
+
+  
+    const index = id
+    const tarefaCadastradas = JSON.parse(localStorage.getItem('tarefas'))
+
+    tarefaCadastradas.splice(index, 1)
+    localStorage.setItem('tarefas', JSON.stringify(tarefaCadastradas))
+    atualiza()
+}
+
+const BotaoDeleta = (atualiza, id) => { 
     const botaoDeleta = document.createElement('button')
 
     botaoDeleta.innerText = 'deletar'
-    botaoDeleta.addEventListener('click', deletarTarefa)
+    botaoDeleta.addEventListener('click', ()=> deletarTarefa (atualiza, id))
 
     return botaoDeleta
-}
-
-const deletarTarefa = (evento) => { 
-    const botaoDeleta = evento.target
-    
-    const tarefaCompleta = botaoDeleta.parentElement
-
-    tarefaCompleta.remove()
-
-    return botaoDeleta
-
 }
 
 export default BotaoDeleta
